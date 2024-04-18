e4fec5fbfe7b95663b89fecb90d65bedeca83f53
Matheus Muniz <i.am.matheus.muniz@gmail.com>
2024-04-17 21:50:31 -0300

    Created a simple gif maker with python

--- gif-maker.py
+++ gif-maker.py
@@ -1,1 +1,9 @@
 import imageio.v3 as iio
+
+filenames = ['sung-jinwoo-1.jpg', 'sung-jinwoo-2.jpg', 'sung-jinwoo-3.jpg']
+images = []
+
+for filename in filenames:
+    images.append(iio.imread(filename))
+
+iio.imwrite('sung-jinwoo.gif', images, duration = 500, loop = 0)
